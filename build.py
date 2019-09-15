df6d82d323436dcea2ba356b49a4e0f795d831cf
johndoe <johnd0e@mail.ua>
2019-09-15 18:32:20 +0300

    build.py: special support of python scripts in pre/postBuild commands
    Stop build if any command failed
    
    Note: I'd like better use [`import_module`], but it is Python3-only.
    So..
    
    [`import_module`](https://docs.python.org/3/library/importlib.html#importlib.import_module)

--- build.py
+++ build.py
@@ -212,9 +212,27 @@ else:
     # no 'dist' folder - so create an empty target folder
     os.makedirs(outDir)
 
+from runpy import run_module
+def run_cmds(cmds):
+    globals = {
+        'buildName': buildName,
+        'settings': settings,
+    }
+    for cmd in cmds:
+        if cmd[-3:] == '.py':
+            run_module(cmd[0:-3], init_globals=globals, run_name='build.py')
+        else:
+            status = os.system(cmd)
+            try:
+                exit_code = os.WIFEXITED(status) and os.WEXITSTATUS(status)
+            except AttributeError: # Windows
+                exit_code = status
+            if exit_code != 0:
+                sys.stderr.write('%s: error: execution failed: %s' % (os.path.basename(sys.argv[0]), cmd))
+                sys.exit(1)
+
 # run any preBuild commands
-for cmd in settings.get('preBuild', []):
-    os.system(cmd)
+run_cmds(settings.get('preBuild',[]))
 
 # load main.js, parse, and create main total-conversion-build.user.js
 main = readfile('main.js')
@@ -287,7 +305,6 @@ if buildMobile:
                         os.path.join(outDir, "IITC_Mobile-%s.apk" % buildMobile))
 
 # run any postBuild commands
-for cmd in settings.get('postBuild', []):
-    os.system(cmd)
+run_cmds(settings.get('postBuild',[]))
 
 # vim: ai si ts=4 sw=4 sts=4 et
